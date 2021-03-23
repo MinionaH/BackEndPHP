f325e0bad1cbf993d09a6d3d832d641c17594da3
Nikolozi <nikoloz.sadirini.1@btu.edu.ge>
2021-03-23 21:35:20 +0400

    Calculator Final

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(){
+
+        return view("test");
+    }
+
+    public function calculate(Request $request){
+        // dd($request);
+        (int)$x = $request->numberOne;
+        (int)$y = $request->numberTwo;
+        $Action = $request->action;
+        if ($Action == 'Addition') {
+            return ($x + $y);
+        }
+        if ($Action == 'Subtraction') {
+            return ($x - $y);
+        }
+        elseif ($Action == 'Multiplication') {
+            return ($x * $y);
+        }
+        else {
+            return ($x / $y);
+        }
+    }
+}
